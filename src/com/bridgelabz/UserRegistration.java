18f012efe59f8bbe12fec92e3cdaa0ae88c97402
Rajkawale7 <Rajkawale712@gmail.com>
2022-04-23 13:25:51 +0530

    Added: Program on User Registration Problem

---
+++ src/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+import java.util.*;
+public class UserRegistration {
+	public static void main(String[] args) {
+		System.out.println("**Welcome to the User Registration Program**");
+	}
+
+}
