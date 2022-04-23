fea821805228795005876f855e80d28598366ac3
Rajkawale7 <Rajkawale712@gmail.com>
2022-04-23 14:40:27 +0530

    Added: This case to check and valide the password

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -5,26 +5,34 @@ import java.util.Scanner;
 public class UserRegistration {
 
 	public static void main(String[] args) {
-		//As a User need to a follow predefined mobile number format
-		Scanner scan = new Scanner (System.in);
-		int ch = 1;
-		do {
-		
-		//Taking input as a mobile number from user
-		System.out.println("Enter Your Mobile Number: ");
-		String MobNumber = scan.next();
+		//As a User need to a follow predefined Unique Password format.
+				Scanner scan = new Scanner (System.in);
+				int ch = 1;
+				do {
+			
+				System.out.println("Enter Your Unique Password: ");
+				String password = scan.next();
 
-		String regex = ("(0/91)?[7-9][0-9]{9}");
-		Pattern p = Pattern.compile(regex);
-		Matcher m = p.matcher(MobNumber);
+				//Compiling the regex pattern for creating password
+				String regex = "^(?=.*[0-9])"
+						+ "(?=.*[a-z])(?=.*[A-Z])"
+		                + "(?=.*[@#$%^&+=])"
+		                + "(?=\\S+$).{8,20}$";
+				Pattern p = Pattern.compile(regex);
+				Matcher m = p.matcher(password);	
 
-		if (m.find()) {
-			System.out.println("Match found");
+				if (m.find()) {
+					System.out.println("Match found");
+				}
+				else {
+					System.out.println("Match not found");
+				}
+				//To continue the same process by using do while loop
+				System.out.println("Continue...press 1,");
+				ch = scan.nextInt();
+				} 
+				while(ch==1);
 		}
-			System.out.println("Continue...press 1,");
-		ch = scan.nextInt();
-		} 
-		while(ch==1);}
 	}
 		
 
